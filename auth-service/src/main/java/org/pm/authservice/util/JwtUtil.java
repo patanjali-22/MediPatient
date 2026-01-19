726acfdfb5a8f8f114d2d52fefbf2b542f686d9c
patanjali-22 <upatanjali6@gmail.com>
2026-01-19 17:59:40 -0500

    added signature exception

--- auth-service/src/main/java/org/pm/authservice/util/JwtUtil.java
+++ auth-service/src/main/java/org/pm/authservice/util/JwtUtil.java
@@ -3,13 +3,13 @@ package org.pm.authservice.util;
 import io.jsonwebtoken.JwtException;
 import io.jsonwebtoken.Jwts;
 import io.jsonwebtoken.security.Keys;
+import io.jsonwebtoken.security.SignatureException;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Component;
 
 import javax.crypto.SecretKey;
 import java.nio.charset.StandardCharsets;
 import java.security.Key;
-import java.security.SignatureException;
 import java.util.Base64;
 import java.util.Date;
 
@@ -39,7 +39,10 @@ public class JwtUtil {
             Jwts.parser().verifyWith((SecretKey) secretKey)
                     .build()
                     .parseSignedClaims(token);
-        } catch (JwtException e) {
+        }catch(SignatureException e){
+            throw new JwtException("Invalid JWT signature");
+        }
+        catch (JwtException e) {
             throw new JwtException("Invalid JWT");
         }
     }
