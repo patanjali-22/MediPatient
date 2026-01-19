3a48a69bb5d743a97b477a8eff3526715c82a3d0
patanjali-22 <upatanjali6@gmail.com>
2026-01-19 17:42:25 -0500

    services are implemented

--- auth-service/src/main/java/org/pm/authservice/controller/AuthController.java
+++ auth-service/src/main/java/org/pm/authservice/controller/AuthController.java
@@ -1,16 +1,17 @@
 package org.pm.authservice.controller;
 
-import com.jayway.jsonpath.internal.path.ArraySliceOperation;
-import jakarta.validation.Valid;
-import org.hibernate.boot.beanvalidation.GroupsPerOperation;
 import org.pm.authservice.dto.LoginRequestDTO;
 import org.pm.authservice.dto.LoginResponseDTO;
 import org.pm.authservice.service.AuthService;
+import io.swagger.v3.oas.annotations.Operation;
+import java.util.Optional;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.*;
-
-import java.util.Optional;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestHeader;
+import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class AuthController {
---
+++ auth-service/src/main/java/org/pm/authservice/service/UserService.java
@@ -0,0 +1,21 @@
+package org.pm.authservice.service;
+
+import org.springframework.stereotype.Service;
+import org.pm.authservice.model.User;
+import org.pm.authservice.repository.UserRepository;
+
+import java.util.Optional;
+
+@Service
+public class UserService {
+
+    private final UserRepository userRepository;
+
+    public UserService(UserRepository userRepository) {
+        this.userRepository = userRepository;
+    }
+
+    public Optional<User> findByEmail(String email) {
+        return userRepository.findByEmail(email);
+    }
+}
