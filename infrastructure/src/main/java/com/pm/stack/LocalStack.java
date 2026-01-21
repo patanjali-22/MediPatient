36b1b507db37cb9bc4b75bba1da5c046561eee83
patanjali-22 <upatanjali6@gmail.com>
2026-01-21 15:17:11 -0500

    localstack IAC code completion for all VPC

--- infrastructure/src/main/java/com/pm/stack/LocalStack.java
+++ infrastructure/src/main/java/com/pm/stack/LocalStack.java
@@ -28,6 +28,7 @@ import software.amazon.awscdk.services.ecs.LogDriver;
 import software.amazon.awscdk.services.ecs.PortMapping;
 import software.amazon.awscdk.services.ecs.Protocol;
 import software.amazon.awscdk.services.ecs.patterns.ApplicationLoadBalancedFargateService;
+import software.amazon.awscdk.services.elasticloadbalancingv2.ApplicationProtocol;
 import software.amazon.awscdk.services.logs.LogGroup;
 import software.amazon.awscdk.services.logs.RetentionDays;
 import software.amazon.awscdk.services.msk.CfnCluster;
@@ -144,8 +145,8 @@ public class LocalStack extends Stack {
     private CfnCluster createMskCluster(){
         return CfnCluster.Builder.create(this, "MskCluster")
                 .clusterName("kafa-cluster")
-                .kafkaVersion("2.8.0")
-                .numberOfBrokerNodes(1)
+                .kafkaVersion("3.8.x")
+                .numberOfBrokerNodes(2)
                 .brokerNodeGroupInfo(CfnCluster.BrokerNodeGroupInfoProperty.builder()
                         .instanceType("kafka.m5.xlarge")
                         .clientSubnets(vpc.getPrivateSubnets().stream()
@@ -242,13 +243,13 @@ public class LocalStack extends Stack {
                                 "SPRING_PROFILES_ACTIVE", "prod",
                                 "AUTH_SERVICE_URL", "http://host.docker.internal:4005"
                         ))
-                        .portMappings(List.of(4004).stream()
-                                .map(port -> PortMapping.builder()
-                                        .containerPort(port)
-                                        .hostPort(port)
+                        .portMappings(List.of(
+                                PortMapping.builder()
+                                        .containerPort(4004)
+                                        .hostPort((4004))
                                         .protocol(Protocol.TCP)
-                                        .build())
-                                .toList())
+                                        .build()
+                        ))
                         .logging(LogDriver.awsLogs(AwsLogDriverProps.builder()
                                 .logGroup(LogGroup.Builder.create(this, "ApiGatewayLogGroup")
                                         .logGroupName("/ecs/api-gateway")
@@ -259,17 +260,18 @@ public class LocalStack extends Stack {
                                 .build()))
                         .build();
 
-
         taskDefinition.addContainer("APIGatewayContainer", containerOptions);
 
-        ApplicationLoadBalancedFargateService apiGateway =
-                ApplicationLoadBalancedFargateService.Builder.create(this, "APIGatewayService")
-                        .cluster(ecsCluster)
-                        .serviceName("api-gateway")
-                        .taskDefinition(taskDefinition)
-                        .desiredCount(1)
-                        .healthCheckGracePeriod(Duration.seconds(60))
-                        .build();
+        ApplicationLoadBalancedFargateService.Builder.create(this, "APIGatewayService")
+                .cluster(ecsCluster)
+                .serviceName("api-gateway")
+                .taskDefinition(taskDefinition)
+                .desiredCount(1)
+                .publicLoadBalancer(true)
+                .healthCheckGracePeriod(Duration.seconds(60))
+                .listenerPort(4004)
+                .protocol(ApplicationProtocol.HTTP)
+                .build();
     }
 
     public static void main(final String[] args) {
